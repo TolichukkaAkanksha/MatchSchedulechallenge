20a7e473d27901006f794f5b8e5f8e4a9a19af58
Tolichukka Akanksha <125852692+TolichukkaAkanksha@users.noreply.github.com>
2023-03-31 09:42:03 +0530

    Add files via upload

---
+++ MatchScheduleoutput.java
@@ -0,0 +1,18 @@
+[Telugu Titans Vs Bengaluru bulls, Telugu Titans Vs Patna pirates, Telugu Titans Vs Bengal Warriors, Telugu Titans Vs U Mumba, Bengaluru bulls Vs Patna pirates, Bengaluru bulls Vs Bengal Warriors, Bengaluru bulls Vs U Mumba, Patna pirates Vs Bengal Warriors, Patna pirates Vs U Mumba, Bengal Warriors Vs U Mumba]
+Point Table
+-------------------------------------------------------------
+Team: Telugu Titans | Played: 4  | Won: 1 | Lost: 3 | Points: 5
+-------------------------------------------------------------
+Team: Bengaluru bulls | Played: 4  | Won: 2 | Lost: 2 | Points: 6
+-------------------------------------------------------------
+Team: Patna pirates | Played: 4  | Won: 4 | Lost: 0 | Points: 8
+-------------------------------------------------------------
+Team: Bengal Warriors | Played: 4  | Won: 1 | Lost: 3 | Points: 5
+-------------------------------------------------------------
+Team: U Mumba | Played: 4  | Won: 2 | Lost: 2 | Points: 6
+Win: 3 points,Lost: 1 points
+
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
